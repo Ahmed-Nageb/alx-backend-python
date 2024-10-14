40b7635a1b74a38c1b4ad9aeb5c53be9c5d307aa
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-10-14 21:49:16 +0300

    0.1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""0-basic"""
+"""The basics of async"""
 import asyncio
 import random
 
 
-async def wait_random(wait_time=10):
+async def wait_random(wait_time: int = 10) -> float:
     """wait_random Function"""
     waiting = random.uniform(0, wait_time)
     await asyncio.sleep(waiting)
